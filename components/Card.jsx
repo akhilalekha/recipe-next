73e447e573fec4d6a237b82971a94b50665331ae
akhilalekha <akhila160@gmail.com>
2021-09-18 23:13:46 +0530

    feat: add card component

---
+++ components/Card.jsx
@@ -0,0 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Card({
+	image,
+	name,
+	calories,
+	time,
+	dietlabels,
+	url,
+	ingredients
+}) {
+	return (
+		<div className="border-2 border-white flex flex-col w-80 h-96 p-4 bg-white m-2 rounded-md shadow-md overflow-y-auto hover:border-blue-600">
+			<Link href={url}>
+				<a>
+					<img
+						className="w-full h-60 rounded-md object-cover"
+						src={image}
+						alt="recipe"
+					/>
+					<div className="">
+						<div className="font-bold text-xl text-left whitespace-pre-wrap">
+							{name}
+
+							{dietlabels.map((label) =>
+								label ? (
+									<span
+										className="inline-block text-sm font-normal bg-green-700 px-2 mx-2 rounded-md text-white whitespace-normal"
+										key={label}
+									>
+										{label}
+									</span>
+								) : null
+							)}
+						</div>
+
+						<div className="flex">
+							<img src="/time.svg" width="15" height="15" alt="time" />
+							<span className="mx-2 font-medium text-sm">{time} min</span>
+						</div>
+
+						<div className="flex">
+							<Image src="/calories.svg" width="15" height="15" alt="" />
+							<p className="mx-2 font-medium text-sm">
+								{Math.floor(calories)} kcal
+							</p>
+						</div>
+					</div>
+				</a>
+			</Link>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,35 @@
 import Head from "next/head";
-// import Image from "next/image";
-// import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import Card from "../components/Card";
+const keys = require("../config/keys");
 
 export default function Home() {
+	const limit = 9;
 	const [items, setItems] = useState([]);
 	const [input, setInput] = useState("");
-	const [query, setQuery] = useState("icecream");
-	// const [loaded, setLoaded] = useState(false);
+	const [query, setQuery] = useState("");
+	const [loaded, setLoaded] = useState(false);
+	const [pg, setPg] = useState({
+		from: 0,
+		to: 9
+	});
 
-	useEffect(() => {
-		let URI = `https://api.edamam.com/search?q=${query}&app_id=${keys.appID}&app_key=${keys.appKey}`;
-		const getRecipes = async () => {
-			const response = await fetch(URI);
-			const data = await response.json();
-			console.log({ data });
-			setItems(data.hits);
-			// setLoaded(true);
-		};
-		console.log("test");
-		getRecipes();
-	}, [query]);
+	// useEffect(() => {
+	// 	console.log("test");
+	// 	getRecipes();
+	// }, [query]);
+
+	const getRecipes = async (from, to) => {
+		let URI = `https://api.edamam.com/search?q=${input}&app_id=${keys.appID}&app_key=${keys.appKey}&from=${from}&to=${to}`;
+		const response = await fetch(URI);
+		const data = await response.json();
+		console.log({ data });
+		setItems(data.hits);
+		if (data) {
+			setLoaded(true);
+		}
+	};
 
 	const handleChange = (e) => {
 		setInput(e.target.value);
@@ -27,44 +37,69 @@ export default function Home() {
 
 	const getSearch = (e) => {
 		e.preventDefault();
-		setQuery(input);
-		setInput("");
+		getRecipes(pg.from, pg.to);
+		// setQuery(input);
+		// setInput("");
 	};
+
+	const handleNext = () => {
+		setPg((prevState) => {
+			return {
+				from: prevState.from + limit,
+				to: prevState.to + limit
+			};
+		});
+		let from = pg.from + limit;
+		let to = pg.to + limit;
+		getRecipes(from, to);
+	};
+
+	const handlePrevious = () => {
+		setPg((prevState) => {
+			return {
+				from: prevState.from - limit,
+				to: prevState.to - limit
+			};
+		});
+		let from = pg.from - limit;
+		let to = pg.to - limit;
+		getRecipes(from, to);
+	};
+
 	return (
-		<div className="bg-green-800">
+		<div className="bg-green-800 h-screen">
 			<Head>
 				<title>Recipe App</title>
 				<meta name="description" content="search and see recipes" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className="flex flex-col justify-center items-center h-full">
-				<div className="px-8 mt-4 w-full">
-					<h1 className="font-semibold text-2xl text-white">
+			<main className="flex flex-col justify-center items-center bg-green-800">
+				<div className="pl-40 mt-4 w-full">
+					<h1 className="font-semibold text-3xl text-white">
 						Search for recipes
 					</h1>
 				</div>
 
-				<form className="flex mx-5" onSubmit={getSearch}>
+				<form className="flex mx-5 w-2/3" onSubmit={getSearch}>
 					<label htmlFor="searchString"></label>
 					<input
 						id="searchString"
 						type="search"
-						className="m-5 px-6 py-2 rounded-md border-none focus:outline-none"
-						// value={input}
+						className="m-5 px-6 py-2 rounded-md border-none focus:outline-none w-full"
+						value={input}
 						placeholder="Search recipes..."
-						// onChange={handleChange}
+						onChange={handleChange}
 					/>
 					<button
-						className="m-5 text-white bg-black px-6 py-2 rounded-md shadow-sm border-none"
+						className="m-5 text-white bg-black px-6 py-2 rounded-md shadow-sm border-none w-32"
 						type="submit"
-						// onClick={getSearch}
 					>
 						Search
 					</button>
 				</form>
 
 				{/* <Popup items={loaded ? items : "nothing"} /> */}
-				{/* <div className="cardContainer">
+				<div className="flex flex-wrap w-full p-14 sm:p-1 justify-center">
 					{items.map((item) => (
 						<Card
 							key={uuidv4()}
@@ -77,7 +112,29 @@ export default function Home() {
 							ingredients={item.recipe.ingredients}
 						/>
 					))}
-				</div> */}
+				</div>
+
+				{loaded && (
+					<div className="">
+						{pg.from > 0 && (
+							<button
+								className="m-5 text-white bg-black px-6 py-2 rounded-md shadow-sm border-none w-32"
+								type="button"
+								onClick={handlePrevious}
+							>
+								Previous
+							</button>
+						)}
+
+						<button
+							className="m-5 text-white bg-black px-6 py-2 rounded-md shadow-sm border-none w-32"
+							type="button"
+							onClick={handleNext}
+						>
+							Next
+						</button>
+					</div>
+				)}
 			</main>
 		</div>
 	);
